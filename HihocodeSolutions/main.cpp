2bb373ca6204dbfb34479cd0c7ada331da0b44ce
NINJA-J <1605563934@qq.com>
2017-09-22 18:55:25 +0800

    Hidocodes

---
+++ HihocodeSolutions/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  HihocodeSolutions
+//
+//  Created by Jonathan闫 on 2017/9/22.
+//  Copyright © 2017年 Jonathan闫. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
